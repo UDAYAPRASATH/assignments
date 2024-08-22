10a5781ba422fb2c84d986270553724a64600597
UDAYAPRASATH <116788882+UDAYAPRASATH@users.noreply.github.com>
2024-08-22 18:51:33 +0530

    Add files via upload
    Week 1 assignments

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+                  
+		
+		String mobileName="Realme";
+		String  mobileModel="GT neo 3";
+		float androidVersion=14.0f;
+		int ram=12;
+		long imeiNumber=57648827820189l;
+		double sarNumber=8237.83837;
+		boolean isChargerAVailable=true;
+		char typeOfCable='C';
+		
+		System.out.println(mobileName);
+		System.out.println(mobileModel);
+		System.out.println(androidVersion);
+		System.out.println(ram);
+		System.out.println(imeiNumber);
+		System.out.println(sarNumber);
+		System.out.println(isChargerAVailable);
+		System.out.println(typeOfCable);
+ System.out.println("----------------------------------------------a");
+		
+		  System.out.println(mobileName+" "+mobileModel+" "+androidVersion+" "+ram+" "+imeiNumber+" "+sarNumber+" "+isChargerAVailable+"\n"+typeOfCable);
+		
+		
+	}
+
+}
