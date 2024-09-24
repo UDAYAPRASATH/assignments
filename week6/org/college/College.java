1bc0e19cbf19a318bb7d19929fa664a0e9eb719c
Udayaprasath <116788882+UDAYAPRASATH@users.noreply.github.com>
2024-09-24 12:45:28 +0530

    Add files via upload

---
+++ week6/org/college/College.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class College {
+	public void collegeName()
+	{
+		System.out.println("KSR College of Engineering");
+	}
+	public void collegeCode()
+	{
+		System.out.println("007");
+	}
+	public void collegeRank()
+	{
+		System.out.println("-3");
+	}
+
+}
